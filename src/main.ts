974fda4e9834f90494f714549dcf836de18ee7cc
Lahiru Yasas <lahiruyasas7@gmail.com>
2026-08-05 14:20:49 +0530

    feat(app): enable global request validation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // strips properties not in the DTO
+      forbidNonWhitelisted: true, // rejects requests with extra properties
+      transform: true, // enables @Type() and auto-transforms payloads
+    }),
+  );
+  await app.listen(3003);
 }
 bootstrap();
