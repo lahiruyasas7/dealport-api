6d9b45feb944ac273324b16584d992c24b4cad9d
Lahiru Yasas <lahiruyasas7@gmail.com>
2026-08-05 22:16:31 +0530

    feat(category): develop get all categories api

---
+++ src/category/categories.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class CategoriesService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  findAll() {
+    return this.prisma.category.findMany({
+      orderBy: { name: 'asc' },
+    });
+  }
+}
