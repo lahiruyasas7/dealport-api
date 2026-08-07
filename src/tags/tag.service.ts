58aa849a6d8af6adb1007f8f741734807cd09fb1
Lahiru Yasas <lahiruyasas7@gmail.com>
2026-08-07 11:09:34 +0530

    feat: develop api to get all tags

---
+++ src/tags/tag.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class TagsService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  findAll() {
+    return this.prisma.tag.findMany({
+      orderBy: { name: 'asc' },
+    });
+  }
+}
